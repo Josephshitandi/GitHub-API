1ad4482ab18849ba005a151b48f9577508a743e4
Josephshitandi <jsphshtnd@gmail.com>
2020-10-05 01:54:53 +0300

    Modifies app-routing module to pick user details as home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { GithubComponent } from './github/github.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const routes: Routes = [
-  { path: 'github', component: GithubComponent},
-  { path: 'homepage', component: HomepageComponent},
+  { path: '', redirectTo: "/homepage", pathMatch: "full" },
+  { path: 'github', component: GithubComponent },
+  { path: 'homepage', component: HomepageComponent },
+
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
