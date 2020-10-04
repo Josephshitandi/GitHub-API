424dd19d6597a7c55c16c748891e72403c9a75da
Josephshitandi <jsphshtnd@gmail.com>
2020-10-04 14:58:11 +0300

    Create getUser function in search user services

--- src/app/search-user/search-user.service.ts
+++ src/app/search-user/search-user.service.ts
@@ -12,30 +12,7 @@ export class SearchUserService {
 
   getUser(term: string) {
     let endPoint = `https://api.github.com/search/users?access_token=${environment.apiKey}&q=${term}`;
-    let promise = new Promise((resolve, reject) => {
-      this.http
-        .get(endPoint)
-        .toPromise()
-        .then(
-          (results) => {
-            this.users = [];
-            for (let i = 0; i < results['items'].length; i++) {
-              let name = results['items'][i]['login'];
-              let image = results['items'][i]['avatar_url'];
-              let github = results['items'][i]['html_url'];
-              let liveLink = results['items'][i]['repos_url'];
-              let user = new Users(i +1, name, image,github,liveLink,);
-              this.users.push(user);
-            }
-            resolve();
-          },
-          (error) => {
-            console.log(error);
-            reject();
-          }
-        );
-    });
-    return promise;
+    
   }
   
   constructor(private http: HttpClient) {}
